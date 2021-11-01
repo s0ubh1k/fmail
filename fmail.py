a6ca329b8a3e569c765598b4eed32192ff8e2383
Druhin <druhinmitra15@gmail.com>
2021-11-01 18:00:31 +0530

    update

--- fmail.py
+++ fmail.py
@@ -1,4 +1,4 @@
-a = open("credentials" , '+r')
+a = open("credentials" , 'r+')
 def add():
     user=input("Enter your name")
     pass_d=input("Enter yout Password")
@@ -6,7 +6,11 @@ def add():
     a.write(user , ":")
     a.write(pass_d+"\n")
     a.close()
+<<<<<<< HEAD
+add(user,pass_d)
+=======
 
+>>>>>>> a70335fd90c6c9392db69a182b190b93d637da82
 def login(user , pass_d):
     
     x = a.read()
